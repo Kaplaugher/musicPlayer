5a74b5127e25eeddbed61429ea9d63c12921a6b3
Kyle <kaplaugher94@gmail.com>
2021-05-08 17:10:01 -0500

    wip

--- jsPractice/minesweeper/minesweeper.js
+++ jsPractice/minesweeper/minesweeper.js
@@ -10,6 +10,7 @@ const TILE_STATUSES = {
 export function createBoard(boardSize, numberOfMines) {
   const board = [];
   const minePositions = getMinePositions(boardSize, numberOfMines);
+  console.log(minePositions);
   for (let x = 0; x < boardSize; x++) {
     const row = [];
     for (let y = 0; y < boardSize; y++) {
@@ -19,7 +20,7 @@ export function createBoard(boardSize, numberOfMines) {
         element,
         x,
         y,
-        mine: true,
+        mine: minePositions.some(positionMatch.bind(null, { x, y })),
         get status() {
           return this.element.dataset.status;
         },
@@ -40,7 +41,35 @@ function getMinePositions(boardSize, numberOfMines) {
   const positions = [];
   while (positions.length < numberOfMines) {
     const position = {
-      x: randomNumber(boardS),
+      x: randomNumber(boardSize),
+      y: randomNumber(boardSize),
     };
+    if (!positions.some((p) => positionMatch(p, position))) {
+      positions.push(position);
+    }
+  }
+  return positions;
+}
+
+function positionMatch(a, b) {
+  return a.x === b.x && a.y === b.y;
+}
+
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+export function markTile(tile) {
+  if (
+    tile.status !== TILE_STATUSES.HIDDEN &&
+    tile.status !== TILE_STATUSES.MARKED
+  ) {
+    return;
+  }
+
+  if (tile.status === TILE_STATUSES.MARKED) {
+    tile.status = TILE_STATUSES.HIDDEN;
+  } else {
+    tile.status = TILE_STATUSES.MARKED;
   }
 }
--- jsPractice/minesweeper/script.js
+++ jsPractice/minesweeper/script.js
@@ -1,16 +1,23 @@
 // UI
-import { createBoard } from './minesweeper.js';
+import { createBoard, markTile } from './minesweeper.js';
 
 const BOARD_SIZE = 2;
 const NUMBER_OF_MINES = 2;
+const minesLeftText = document.querySelector('[data-mine-count]');
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
 console.log(board);
 
 const boardElement = document.querySelector('.board');
 boardElement.style.setProperty('--size', BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
 board.forEach((row) => {
   row.forEach((tile) => {
     boardElement.append(tile.element);
+    tile.element.addEventListener('click', () => {});
+    tile.element.addEventListener('contextmenu', (e) => {
+      e.preventDefault();
+      markTile(tile.element);
+    });
   });
 });
